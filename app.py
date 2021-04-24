b44a68f71cd4b39ba5e39b20f34c95ea2c891db8
joelox87 <j.aponte87@gmail.com>
2021-04-24 08:56:17 -0500

    updates

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # Import Dependencies
-import datetime as dt
 import numpy as np
 import pandas as pd
 import datetime as dt
 import sqlalchemy
+from flask import Flask, jsonify
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+
+# Reflect Database into ORM classes
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect = True)
